3e888a0fe64074eb158038bc3df3f1c70707f5be
Zhi Ye <zhi.ye@mimik.com>
2020-12-02 10:55:39 -0800

    EPOC-374 add image url download support to mdeployment agent

--- src/processors/imageProcessor.js
+++ src/processors/imageProcessor.js
@@ -1,42 +1,52 @@
 const rp = require('request-promise');
 const fs = require('fs-extra');
+const uuid = require('uuid');
 
 const { getRichError } = require('@bananabread/response-helper');
 
 const deployImage = (imageDetails, correlationId) => {
   const { imageLink, deploymentLink, service } = imageDetails;
+  const fileName = `${uuid.v4()}.tar`;
+  const filePath = `./${fileName}`;
+
+  const removeImageFile = () => fs.remove(filePath)
+    .catch((err) => {
+      throw getRichError('System', 'cannot remove image from file directory', { filePath }, err, false, correlationId);
+    });
+
   return rp({
     uri: imageLink.url,
     method: imageLink.method,
   })
-    .then((result) => {
-      const filePath = `./${service.name}-${service.version}.tar`;
-      return fs.outputFile(filePath, result)
-        .then(() => {
-          const options = {
-            uri: deploymentLink.url,
-            headers: {
-              ...deploymentLink.headers,
-            },
-            method: deploymentLink.method,
-            formData: {
-              image: {
-                value: fs.createReadStream(filePath),
-                options: {
-                  filename: `${service.name}-${service.version}.tar`,
-                },
+    .then((result) => fs.outputFile(filePath, result)
+      .then(() => {
+        const options = {
+          uri: deploymentLink.url,
+          headers: {
+            ...deploymentLink.headers,
+          },
+          method: deploymentLink.method,
+          formData: {
+            image: {
+              value: fs.createReadStream(filePath),
+              options: {
+                filename: fileName,
               },
             },
-          };
-          return options;
-        });
-    })
+          },
+        };
+        return options;
+      }))
     .catch((err) => {
       throw getRichError('System', 'cannot fetch image from the imageLink', { imageLink, service }, err, false, correlationId);
     })
     .then(rp)
-    .then(JSON.parse)
+    .then((deploymentStatus) => {
+      removeImageFile();
+      return JSON.parse(deploymentStatus);
+    })
     .catch((err) => {
+      removeImageFile();
       throw getRichError('System', 'cannot deploy image to deploymentLink', { deploymentLink, service }, err, false, correlationId);
     });
 };
